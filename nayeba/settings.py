06ed8b023344cdba0ccfd343839e299f54cb4d32
Sédrick Gael KOUAGNI <kanangamansedrickgael@gmail.com>
2023-06-28 16:32:06 +0000

    Update settings.py

--- nayeba/settings.py
+++ nayeba/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5seeh5zwvyt%st__vihk(t4bj03x+w3&v107m0qe)esxw_56b-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
